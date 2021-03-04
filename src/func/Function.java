caecfdc6ef6eae5469dbecb768d2681598172f4e
younsik.choi <nadays3019@gmail.com>
2021-03-04 20:17:15 +0900

    곱셈구현

--- src/func/Function.java
+++ src/func/Function.java
@@ -2,4 +2,28 @@ package func;
 
 public class Function implements IFunction {
 
+	@Override
+	public int add(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int sub(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public int mul(int a, int b) {
+		int mul = a *b;
+		return mul;
+	}
+
+	@Override
+	public double div(int a, int b) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
 }
